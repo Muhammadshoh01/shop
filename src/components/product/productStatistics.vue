587e48db31a6b0fcd975c8b8a5e5f21698771342
Muhammadshoh01 <rajabovmuhammadshoh009@gmail.com>
2023-09-14 11:47:22 +0500

    fix

--- src/components/product/productStatistics.vue
+++ src/components/product/productStatistics.vue
@@ -6,15 +6,6 @@
 					<template #title>
 						<div style="display: inline-flex; align-items: center">
 							Umumiy summa
-							<el-tooltip
-								effect="dark"
-								content="Number of users who logged into the product in one day"
-								placement="top"
-							>
-								<el-icon style="margin-left: 4px" :size="12">
-									<Warning />
-								</el-icon>
-							</el-tooltip>
 						</div>
 					</template>
 				</el-statistic>
